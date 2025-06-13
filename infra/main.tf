12b2efe15d12f2f3378c875c067b4902838716b0
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2025-06-13 09:39:10 +0900

    add alb con

--- infra/main.tf
+++ infra/main.tf
@@ -10,23 +10,23 @@ module "vpc" {
   single_nat_gateway = true
 }
 
-# module "eks" {
-#   source          = "terraform-aws-modules/eks/aws"
-#   version         = "20.8.3"
-#   cluster_name    = var.cluster_name
-#   cluster_version = "1.29"
-#   subnet_ids      = module.vpc.private_subnets
-#   vpc_id          = module.vpc.vpc_id
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "20.8.3"
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
 
-#   eks_managed_node_groups = {
-#     default = {
-#       instance_types = ["m6i.large"]
-#       min_size       = 1
-#       max_size       = 1
-#       desired_size   = 1
-#     }
-#   }
-# }
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["m6i.large"]
+      min_size       = 1
+      max_size       = 1
+      desired_size   = 1
+    }
+  }
+}
 
 resource "aws_security_group" "redis" {
   name        = "redis-sg"
@@ -92,7 +92,7 @@ resource "aws_db_instance" "oracle" {
   max_allocated_storage  = 500
   storage_type           = "gp3"
   storage_encrypted      = true
-  multi_az               = true
+  multi_az               = false
   license_model         = "license-included"
 
   username               = "admin"
@@ -110,6 +110,29 @@ resource "aws_db_instance" "oracle" {
 
   backup_retention_period = 7
   backup_window           = "03:00-04:00"
+}
+
+data "aws_eks_cluster" "eks" {
+   name = module.eks.cluster_name
+}
+   
+module "iam_eks_alb_controller" { 
+  source = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc" 
+  version = "5.38.0" 
+  create_role = true 
+  role_name = "${var.cluster_name}-alb-controller" 
+  provider_url = replace(data.aws_eks_cluster.eks.identity[0].oidc[0].issuer, "https://", "") 
+  role_policy_arns = ["arn:aws:iam::aws:policy/AWSLoadBalancerControllerIAMPolicy"] 
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:aws-load-balancer-controller"]
+}
+    
+resource "kubernetes_service_account" "alb_controller" { 
+  metadata { 
+    name = "aws-load-balancer-controller" 
+    namespace = "kube-system" 
+    labels = { "app.kubernetes.io/name" = "aws-load-balancer-controller" } 
+    annotations = { "eks.amazonaws.com/role-arn" = module.iam_eks_alb_controller.iam_role_arn }
+  }
 }
 
 
